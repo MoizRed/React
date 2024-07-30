0a762d679b2490aa0d5c4eefef97227a9780cdf2
MoizRed <abdoureduniv2000@gmail.com>
2024-07-30 17:24:57 +0100

    yes

--- react/src/Mycomponent.jsx
+++ react/src/Mycomponent.jsx
@@ -2,25 +2,16 @@ import {useEffect, useState} from 'react'
  
 function Mycomponent() {
 
-  const update = () =>{
-    setName(data.name);
-    setdescription(data.description);
-    
-  }
-
-
-
-
   let data ;
-
-
 const [name , setName] = useState("null animal name");
 const [info , setdescription]  = useState("null animal descriptor");
 const [x , setX ] = useState("1")
 
-
- 
-
+const update = () =>{
+  setName(data.name);
+  setdescription(data.description);
+  
+}
   useEffect( () => {
 
     const fetchdata = async () => {
@@ -31,58 +22,30 @@ const [x , setX ] = useState("1")
            console.log(data)
          }catch(error){console.log(error)}
          
-         
           }
-   
          fetchdata()
 
-        
    },[x]
   
   )
   
-
- 
-
-
   return (
-
     <div>
-
    <p> name : {name} </p>
-
-
-<br></br>
-
-
    <p> info: {info}</p> 
-
-
-
-
-
 
    <button onClick ={ ()=> {
 
 var xx = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
 
-console.log(xx)
+        console.log(xx)
         setX(xx)
-    update()
-
-
-
+        update()
 }
-
-
 
 }> update </button>
    
     </div>
   ) 
 }
-
-
-
-
 export default Mycomponent
