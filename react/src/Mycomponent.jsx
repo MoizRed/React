4af5629d5ba28ef6d636b17e0c68f75ccf7c5fb8
MoizRed <abdoureduniv2000@gmail.com>
2024-07-30 17:20:43 +0100

    Success

---
+++ react/src/Mycomponent.jsx
@@ -0,0 +1,88 @@
+import {useEffect, useState} from 'react'
+ 
+function Mycomponent() {
+
+  const update = () =>{
+    setName(data.name);
+    setdescription(data.description);
+    
+  }
+
+
+
+
+  let data ;
+
+
+const [name , setName] = useState("null animal name");
+const [info , setdescription]  = useState("null animal descriptor");
+const [x , setX ] = useState("1")
+
+
+ 
+
+  useEffect( () => {
+
+    const fetchdata = async () => {
+      console.log("got the change")
+     try{
+     const res = await fetch(`https://freetestapi.com/api/v1/animals/${x}`)
+          data = await res.json()
+           console.log(data)
+         }catch(error){console.log(error)}
+         
+         
+          }
+   
+         fetchdata()
+
+        
+   },[x]
+  
+  )
+  
+
+ 
+
+
+  return (
+
+    <div>
+
+   <p> name : {name} </p>
+
+
+<br></br>
+
+
+   <p> info: {info}</p> 
+
+
+
+
+
+
+   <button onClick ={ ()=> {
+
+var xx = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
+
+console.log(xx)
+        setX(xx)
+    update()
+
+
+
+}
+
+
+
+}> update </button>
+   
+    </div>
+  ) 
+}
+
+
+
+
+export default Mycomponent
